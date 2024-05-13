31bef5205e483e857a3fc59349a96efab2c214ea
pbirks <110080095+pbirks@users.noreply.github.com>
2024-05-13 15:02:37 -0500

    Initial Commit

---
+++ todoapp/src/components/TodoList.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function TodoList() {
+  let todos = [
+    'Go to the gym',
+    'Eat more fruits and vege',
+    'Pick up the kids from school'
+  ]
+  
+  return (
+    <ul class="main">
+      {todos.map((todo, todoIndex) => {
+        return (
+           <li className='todoItem' key={todoIndex}>{todo}</li> 
+        )
+      })}
+    
+    </ul>
+  )
+}
+
